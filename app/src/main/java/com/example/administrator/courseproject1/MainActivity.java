f0b62f599d8c768eb4d50f80cf74b837ae909aec
savelgmx <savel@gmx.com>
2018-04-20 15:52:49 +0700

    Course Project#1 Comment с содержанием задания

--- app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
+++ app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.courseproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +18,32 @@ public class MainActivity extends AppCompatActivity {
 Если в EditText нет текста, то по нажатию на Button ничего не происходит.
  */
 
+private EditText mEditTextOne;
+private Button mButtonOne;
+
+//-----обработка нажатия на кнопки
+    private View.OnClickListener mOnButtonOneClickListener= new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            //todo обработка нажатия на кнопки
+        }
+
+};
+    private boolean IsEditTextEmpty(){
+        return !TextUtils.isEmpty(mEditTextOne.getText());
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //------инициализируем EditText и Button----
+        mEditTextOne=findViewById(R.id.etOne);
+        mButtonOne=findViewById(R.id.buttonOne);
+        //-------иницыализируем обработчик нажатия на кнопки
+        mButtonOne.setOnClickListener(mOnButtonOneClickListener);
+
     }
 }
