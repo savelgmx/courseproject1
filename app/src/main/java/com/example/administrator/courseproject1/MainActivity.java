73940dd8482122ec5a4c65231c01e46672d7d1a2
savelgmx <savel@gmx.com>
2018-04-20 13:02:28 +0700

    Course Project#1 Comment с содержанием задания

--- app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
+++ app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
@@ -5,6 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+/*
+Задание
+
+Создать проект с пустой активити.
+В разметку добавить EditText и Button.
+Вводим текст и по нажатию на Button отображаем Toast с введенным текстом.
+Если в EditText нет текста, то по нажатию на Button ничего не происходит.
+ */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
