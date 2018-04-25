9e790a833a823e073df77902ec0ac6e0b5ff7dba
savelgmx <savel@gmx.com>
2018-04-25 13:46:27 +0700

    Course Project#1 changes from 25.04.2018 Введены лайоуты для второй активити и класс SecondActivity

---
+++ app/src/main/java/com/example/administrator/courseproject1/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.administrator.courseproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 25.04.2018.
+ */
+
+public class SecondActivity extends AppCompatActivity{
+
+
+    private TextView
+    private Button mButtonTwo;
+
+    @Override
+
+    protected void OnCreate(){}
+
+}
