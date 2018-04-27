47314e36a4f80486dab959f83714d9fb23bea52b
savelgmx <savel@gmx.com>
2018-04-27 13:40:30 +0700

    Course Project#1 changes from 25.04.2018 Введены лайоуты для второй активити и класс SecondActivity

--- app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
+++ app/src/main/java/com/example/administrator/courseproject1/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.administrator.courseproject1;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -30,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
 private EditText mEditTextOne;
 private Button mButtonOne;
+private String mEditTextOneString;//произвольная строка в mEditTextOneString
 
+    private void showMessage(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
 
 //-----обработка нажатия на кнопки
     private View.OnClickListener mOnButtonOneClickListener;
@@ -39,13 +46,24 @@ private Button mButtonOne;
         mOnButtonOneClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //todo обработка нажатия на кнопки
 
                 if (IsEditTextEmpty())  //если текст в EditText присутсвует то выдает подсказку на экран
 
                 {
-                    Toast.makeText(getApplicationContext(),mEditTextOne.getText().toString(),Toast.LENGTH_LONG).show();
-                 }
+                    mEditTextOneString=mEditTextOne.getText().toString();
+                    showMessage(mEditTextOneString);
+                    //далее запускаем вторую форму с помощью Intent
+                    Intent startSecondActivityIntent =new Intent(MainActivity.this,SecondActivity.class);
+                    startSecondActivityIntent.putExtra(SecondActivity.MAIN_EDITTEXT_KEY,mEditTextOneString);//Intent принимает текст введенный в EditText первого экрана.
+                    startActivity(startSecondActivityIntent);//собственно старт второй формы через интент
+ /*                   */
+
+                 } else {
+
+                    mEditTextOneString=getResources().getString(R.string.input_error);
+                    showMessage(mEditTextOneString);//выдача сообщения обшибке ввода
+                }
+
 
             }
 
@@ -58,15 +76,15 @@ private Button mButtonOne;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //------инициализируем EditText и Button----
         mEditTextOne=findViewById(R.id.etOne);
         mButtonOne=findViewById(R.id.buttonOne);
-        //-------иницыализируем обработчик нажатия на кнопки
-        mButtonOne.setOnClickListener(mOnButtonOneClickListener);
+
+        mButtonOne.setOnClickListener(mOnButtonOneClickListener);//иницыализируем обработчик нажатия на кнопки
 
     }
 }
