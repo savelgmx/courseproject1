52d249add32a420f14338dae55f43768ea9286c5
Andrey Savitsky <savel@gmx.com>
2018-04-29 16:31:51 +0700

    Course Project#1 changes from 28.04.2018 Задание 3 Задание 3. (выполнено частично 28.04 градиент добавлен) Добавить любой градиент в качестве фона обоим экранам. По нажатию на Button второго экрана открывается браузер со страницей google.com с вбитым в поиск словом, которое отображается в TextView.

--- app/src/main/java/com/example/administrator/courseproject1/SecondActivity.java
+++ app/src/main/java/com/example/administrator/courseproject1/SecondActivity.java
@@ -2,6 +2,8 @@ package com.example.administrator.courseproject1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +18,35 @@ public class SecondActivity extends AppCompatActivity{
 
     private TextView mTextView;
     private Button mButtonTwo;
+    private WebView webView;
+    private String webViewSearchPage;
+
+    //-----обработка нажатия на кнопки
+    private View.OnClickListener mOnButtonTwoClickListener;
+
+    {
+        mOnButtonTwoClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                //теперь webview
+                webView = (WebView) findViewById(R.id.webview);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webViewSearchPage="https://www.google.com/search?q="+mTextView.getText();
+
+                webView.loadUrl(webViewSearchPage);//webView.loadUrl("http://www.google.com");
+
+            }
+
+        };
+
+
+
+    }
+
+
+
 
     @Override
 
@@ -25,8 +56,13 @@ public class SecondActivity extends AppCompatActivity{
 
         mTextView =findViewById(R.id.tvTwo);
         Bundle bundle =getIntent().getExtras();
-        mTextView.setText(bundle.getString(MAIN_EDITTEXT_KEY));
+        mTextView.setText(bundle.getString(MAIN_EDITTEXT_KEY));//в нее будем записывать критерий поиска
+
+        mButtonTwo.setOnClickListener(mOnButtonTwoClickListener);
+
+
     }
+
 
 
 }
